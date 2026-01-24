16e29f3d9424902a60968ac4c55f60fee5c6a3bf
earnwithalee <earnwithalee@gmail.com>
2026-01-25 03:11:23 +0500

    chore: configure global toast provider

--- frontend/components/ClientProviders.tsx
+++ frontend/components/ClientProviders.tsx
@@ -3,8 +3,14 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
+import { ToastProvider } from "./Toast";
+
 const Providers = dynamic(() => import("./Providers"), { ssr: false });
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
-    return <Providers>{children}</Providers>;
+    return (
+        <Providers>
+            <ToastProvider>{children}</ToastProvider>
+        </Providers>
+    );
 }
