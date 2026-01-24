526377e92af543e85492db3735539c4392b317b7
earnwithalee <earnwithalee@gmail.com>
2026-01-25 03:17:03 +0500

    feat(page): integrate social hub into landing page

--- frontend/components/LandingPage.tsx
+++ frontend/components/LandingPage.tsx
@@ -1,10 +1,8 @@
-"use client";
-
-import React from "react";
-import Image from "next/image";
+import Link from "next/link";
 import dynamic from "next/dynamic";
 import { BadgeCheck, Zap, Trophy, ArrowRight, Github, LayoutDashboard } from "lucide-react";
 import Leaderboard from "./Leaderboard";
+import SocialHub from "./SocialHub";
 
 const WalletConnect = dynamic(() => import("./WalletConnect"), { ssr: false });
 const GithubScorer = dynamic(() => import("./GithubScorer"), { ssr: false });
